687a5a9adb70a180e44fef874c9e04abbf8212f6
Duc Manh Nguyen <ndmanh3003@gmail.com>
2024-08-24 16:22:58 +0700

    refactor(server): keep alive

--- utils/keep.ts
+++ utils/keep.ts
@@ -2,21 +2,26 @@ import axios from 'axios'
 
 export const keepAlive = async () => {
   const callEnv = process.env.CALL
-  const timeInterval = process.env.TIME_INTERVAL
-
-  if (!callEnv || !timeInterval) return
+  if (!callEnv) return
 
   const urls = callEnv.split('/@/')
 
   const fetchUrls = async (urls: string[]) => {
-    const promises = urls.map((url) => axios.get(url))
-    await Promise.all(promises)
+    for (const url of urls) {
+      console.log(`Calling: ${url}`)
+      try {
+        const response = await axios.get(url)
+        console.log(`URL ${url} was successfully called. Status: ${response.status}`)
+      } catch (error) {
+        console.log(`URL ${url} encountered an error: ${error}`)
+      }
+    }
   }
 
   setInterval(
     () => {
       fetchUrls(urls).catch(() => {})
     },
-    parseInt(timeInterval || '300000', 10)
+    parseInt(process.env.timeInterval || '300000', 10)
   )
 }
